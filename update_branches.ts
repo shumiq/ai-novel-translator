c54e8752777384f7acd9501e099737994247597e
IQ <santiphap.wat@gmail.com>
2026-05-10 10:32:33 +0700

    feat: add script to rebase web and epub branches with new chapters

---
+++ update_branches.ts
@@ -0,0 +1,47 @@
+import { execSync } from "child_process";
+import { Logger } from "./utils/logger";
+
+const webBranches = execSync(`git branch --list "web/*"`, { encoding: "utf-8" })
+  .split("\n")
+  .map((branch) => branch.trim())
+  .filter((branch) => branch);
+
+const epubBranches = execSync(`git branch --list "epub/*"`, {
+  encoding: "utf-8",
+})
+  .split("\n")
+  .map((branch) => branch.trim())
+  .filter((branch) => branch);
+
+execSync(`git checkout main -f`);
+
+for (const branch of [...webBranches, ...epubBranches]) {
+  Logger.info(`Rebase branch ${branch}...`);
+  execSync(`git checkout ${branch} -f`);
+  execSync(`git rebase main`);
+  if (branch.startsWith("web/")) {
+    Logger.info(`Fetch new chapters for branch ${branch}...`);
+    execSync(`git checkout ${branch} -f`);
+    execSync(`bun prepare.ts`);
+    const changes = execSync(`git status --porcelain`, { encoding: "utf-8" })
+      .trim()
+      .split("\n")
+      .filter((line) => line);
+    if (changes.length > 0) {
+      Logger.info(
+        `Committing ${changes.length} changes for branch ${branch}...`,
+      );
+      execSync(`git add .`);
+      const lastCommitMessage = execSync(`git log -1 --pretty=%B`, {
+        encoding: "utf-8",
+      }).trim();
+      if (lastCommitMessage === "wip") {
+        execSync(`git commit --amend --no-edit`);
+      } else {
+        execSync(`git commit -m "wip"`);
+      }
+    }
+  }
+}
+
+execSync(`git checkout main -f`);
